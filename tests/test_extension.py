489698afc129511bb286080b9c0d065bd49eb510
Friedrich Lindenberg <friedrich@pudo.org>
2018-05-03 19:10:13 +0200

    add extension tests

---
+++ tests/test_extension.py
@@ -0,0 +1,15 @@
+import unittest
+
+from celestial import normalize_extension
+
+
+class ExtensionTest(unittest.TestCase):
+
+    def test_normalize(self):
+        self.assertEqual(normalize_extension('.doc'), 'doc')
+        self.assertEqual(normalize_extension(None), None)
+        self.assertEqual(normalize_extension(''), None)
+        self.assertEqual(normalize_extension('bla.doc'), 'doc')
+        self.assertEqual(normalize_extension('bla.DOC'), 'doc')
+        self.assertEqual(normalize_extension('bla.DO C'), 'doc')
+        self.assertEqual(normalize_extension('bla.  DOC  '), 'doc')
