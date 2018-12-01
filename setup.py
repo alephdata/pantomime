7520becc95d33a17e491aef7c17a3ea68901d4fc
Friedrich Lindenberg <friedrich@pudo.org>
2018-12-01 16:40:15 +0100

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pantomime',
-    version='0.3.0',
+    version='0.3.1',
     description="MIME type normalisation and labels.",
     long_description="",
     classifiers=[
