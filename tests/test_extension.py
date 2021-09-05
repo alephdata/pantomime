396752d6e5462088456e34a691fc93ba034361f6
Friedrich Lindenberg <friedrich@pudo.org>
2021-09-05 14:36:10 +0200

    black the tests folder

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -5,24 +5,23 @@ from pantomime.filename import mimetype_extension as mime_ext
 
 
 class ExtensionTest(unittest.TestCase):
-
     def test_normalize(self):
-        self.assertEqual(normalize_extension('.doc'), 'doc')
+        self.assertEqual(normalize_extension(".doc"), "doc")
         self.assertEqual(normalize_extension(None), None)
-        self.assertEqual(normalize_extension(''), None)
-        self.assertEqual(normalize_extension('bla.doc'), 'doc')
-        self.assertEqual(normalize_extension('bla.DOC'), 'doc')
-        self.assertEqual(normalize_extension('bla.DO C'), 'doc')
-        self.assertEqual(normalize_extension('bla.  DOC  '), 'doc')
+        self.assertEqual(normalize_extension(""), None)
+        self.assertEqual(normalize_extension("bla.doc"), "doc")
+        self.assertEqual(normalize_extension("bla.DOC"), "doc")
+        self.assertEqual(normalize_extension("bla.DO C"), "doc")
+        self.assertEqual(normalize_extension("bla.  DOC  "), "doc")
 
-        self.assertEqual(normalize_extension('TXT'), 'txt')
-        self.assertEqual(normalize_extension('.TXT'), 'txt')
-        self.assertEqual(normalize_extension('foo.txt'), 'txt')
-        self.assertEqual(normalize_extension('foo..TXT'), 'txt')
-        self.assertEqual(normalize_extension('.HTM,L'), 'html')
+        self.assertEqual(normalize_extension("TXT"), "txt")
+        self.assertEqual(normalize_extension(".TXT"), "txt")
+        self.assertEqual(normalize_extension("foo.txt"), "txt")
+        self.assertEqual(normalize_extension("foo..TXT"), "txt")
+        self.assertEqual(normalize_extension(".HTM,L"), "html")
 
     def test_mimetype_extension(self):
         self.assertEqual(mime_ext(None), None)
-        self.assertEqual(mime_ext(''), None)
-        self.assertEqual(mime_ext('bla'), None)
-        self.assertEqual(mime_ext('application/pdf'), 'pdf')
+        self.assertEqual(mime_ext(""), None)
+        self.assertEqual(mime_ext("bla"), None)
+        self.assertEqual(mime_ext("application/pdf"), "pdf")
