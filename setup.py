8cc742362dae836959f7ecf10a7f35301059b98f
Friedrich Lindenberg <friedrich@pudo.org>
2021-09-05 14:45:56 +0200

    Bump version: 0.4.2 → 0.5.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="pantomime",
-    version="0.4.2",
+    version="0.5.0",
     description="MIME type normalisation and labels.",
     long_description=long_description,
     long_description_content_type="text/markdown",
